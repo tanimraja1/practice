caff1b78b8db556f4626d4d42827876aa8630553
kartishdr <46895633+kartishdr@users.noreply.github.com>
2019-02-01 19:17:54 +0530

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-sudo cat <<EOF > /etc/yum.repos.d/nginx.repo 
+sudo cat << EOF > /etc/yum.repos.d/nginx.repo 
 [nginx] 
 name=nginx repo 
 baseurl=http://nginx.org/packages/mainline/centos/7/$basearch/
 gpgcheck=0
 enabled=1
 EOF 
+
 sudo yum install nginx
