d9bb6834c2a8f24d8026a6e43e6d424e357c17b9
kartishdr <46895633+kartishdr@users.noreply.github.com>
2019-02-01 16:40:51 +0530

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -1,11 +1,9 @@
-#cloud-config
-package_upgrade: true
-runcmd:
- - echo "Created by Azure terraform-vmss-cloudinit module." | sudo cat <<EOF > /etc/yum.repos.d/nginx.repo \
+#!/bin/bash
+sudo cat <<EOF > /etc/yum.repos.d/nginx.repo \
 [nginx] \
 name=nginx repo \
 baseurl=http://nginx.org/packages/mainline/centos/7/$basearch/ \
 gpgcheck=0 \
 enabled=1 \
-EOF \
-&& sudo yum install nginx \
+EOF 
+sudo yum install nginx \
